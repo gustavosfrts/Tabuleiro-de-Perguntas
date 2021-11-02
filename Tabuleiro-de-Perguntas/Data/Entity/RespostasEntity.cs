87ffd510f1a04ced5b4d04c4e0899556eca9312e
Gustavo da Silva Freitas <52016089+gustavosfrts@users.noreply.github.com>
2021-11-01 21:29:45 -0300

    Delete Tabuleiro-de-Perguntas directory

--- Tabuleiro-de-Perguntas/Data/Entity/RespostasEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace Tabuleiro_de_Perguntas.Data.Entity
-{
-    public class RespostasEntity
-    {
-        public int Id { get; set; }
-        public string pergunta { get; set; }
-        public int nivel_pergunta { get; set; }
-    }
-}
