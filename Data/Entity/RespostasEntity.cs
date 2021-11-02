512e8c1b2066487e97afdd536bdfb3d425ef61b6
Gustavo da Silva Freitas <52016089+gustavosfrts@users.noreply.github.com>
2021-11-01 21:31:01 -0300

    Add files via upload

---
+++ Data/Entity/RespostasEntity.cs
@@ -0,0 +1,9 @@
+namespace Tabuleiro_de_Perguntas.Data.Entity
+{
+    public class RespostasEntity
+    {
+        public int Id { get; set; }
+        public string pergunta { get; set; }
+        public int nivel_pergunta { get; set; }
+    }
+}
